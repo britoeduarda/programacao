07d3e282504062b3cb9ea87d0b8f84e3b00f53ca
britoeduarda <111579961+britoeduarda@users.noreply.github.com>
2023-11-06 08:46:39 -0300

    Add files via upload

---
+++ repositorio-maria-main/main.js
@@ -0,0 +1,10 @@
+function tocaSom(idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
+}
+
+const listaDeteclas=document.querySelectorAll('.tecla');
+
+//para
+for(let contador=0;contador<listaDeteclas.length;contador++)
+
+const tecla=listaDeteclas(contador);
